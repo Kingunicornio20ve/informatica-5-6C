e53f6fd6365915f623bed99160a86e9459f56c7f
Kingunicornio20ve <qqyjdwtq4n@privaterelay.appleid.com>
2026-08-13 20:20:13 +0000

    strory

--- story.py
+++ story.py
@@ -1,18 +1,29 @@
 def main():
-  planet = input("Planet:")
 
-  # Separation
-  print("Hello", planet)
+ # planet = input("Planet:")
 
-  # Ending
-  print("Hello", end=" ")
-  print(planet)
+   # Separation
+   # print("Hello", planet)
+
+   # Ending
+   # print("Hello", end=" ")
+   # print(planet)
 
   # Concatenation
-  print("Hello " + planet)
+   # print("Hello " + planet)
 
-  # Formatted String
-  print(f"Hello {planet}")
+    # Formatted String
+  # print(f"Hello {planet}")
+  name = input("What's your name?").title().strip()
+  color = input("Tell me a color: ").lower().strip()
+  adj = input("Give me a adj: ").lower().strip()
+  goal = input("Tell me a goal: ").lower().strip()
+
+  print(f"Hello {name}!")
+  print()
+
+ print("This is your story:")
+  print(f"At dawn the sky turned {color}, and the air felt {adj}. I decided today I will finally {goal}.")
 
 if __name__ == "__main__":
   main()
