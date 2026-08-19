b8cb6f86d6730930ce0543cb0ea8f00e6b709a3a
Kingunicornio20ve <qqyjdwtq4n@privaterelay.appleid.com>
2026-08-19 20:15:15 +0000

    currency

--- currency.py
+++ currency.py
@@ -1,4 +1,15 @@
-def main()
-    pesos = int(input("What do you have left in pesos?:"))
-    soles = int(input("What do you have left i soles?:" ))
-    
+def main():
+    p = float(input("What do you have left in pesos?: "))
+    s = float(input("What do you have left in soles?: "))
+    r = float(input("what do you have left in reais?: "))
+
+    mxn = (p * 0.0054) + (s * 5.07) + (r * 3.28)
+    usd = mxn / 17.06
+
+    print("USD:", round(usd, 2))
+    print("MXN:", round(mxn, 2))
+
+if __name__ == "__main__":
+    main()
+
+
