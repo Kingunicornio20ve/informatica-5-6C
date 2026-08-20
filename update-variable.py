8f6f68e4d3d5de1aa0f622398211d46ec8d25c7c
Kingunicornio20ve <qqyjdwtq4n@privaterelay.appleid.com>
2026-08-20 16:40:07 +0000

    variable

---
+++ update-variable.py
@@ -0,0 +1,32 @@
+def main():
+ # Augmented Assigment Operator
+
+ money = 5
+ money += 10
+ print(money) # This will print 15
+
+  # Substraction Assigment Operator
+
+ miutes = 60
+ minutes -= 25
+ print(minutes) # This will print 35
+
+  # Multiply Assigment Operator
+
+ text = "Im Allison"
+ text *= 20
+ print(text)
+
+  # Division Assigment Operator
+
+ pizzas = 8
+ people = int(input("Number of people at the pizza party: "))
+ pizzas /= people
+ print(pizzas)
+
+ # Modulos Assigmnment Operator
+ leftover = 8
+ leftover %= people
+ print(leftover)
+if __name__== "__main__":
+    main()
