8aebf9718327e74f6b27e01251bb3635c87e1f0b
Kingunicornio20ve <qqyjdwtq4n@privaterelay.appleid.com>
2026-08-24 19:33:48 +0000

    password

---
+++ password.py
@@ -0,0 +1,15 @@
+def main():
+    password = "allisonqueen20"
+
+    attempt = input("Enter the password: ")
+    if attempt == password:
+       print("Correct password.")
+
+    print("See you later.")
+
+
+
+
+
+if __name__ =="__main__":
+    main()
