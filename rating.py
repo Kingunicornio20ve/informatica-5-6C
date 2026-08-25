d2663893b54d4f5532f248a832204c1b8a0f0238
Kingunicornio20ve <qqyjdwtq4n@privaterelay.appleid.com>
2026-08-25 16:39:55 +0000

    restaurant

---
+++ rating.py
@@ -0,0 +1,18 @@
+def maain():
+    print("ALLI'C")
+    rating = float(input("Plis rating or restaurant with numbers 0 to 5: "))
+
+    if rating > 4.5:
+        print("Perfection")
+        
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
