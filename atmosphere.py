827b6621d2c4e740e7d2cb5df283e575ebd51028
Kingunicornio20ve <qqyjdwtq4n@privaterelay.appleid.com>
2026-08-27 17:27:19 +0000

    test

--- atmosphere.py
+++ atmosphere.py
@@ -1,8 +1,21 @@
 def main():
-    layer = input("Descent atmosphere layer : ")
+    atmosphere = input("Descent atmospher layer : ")
+    altitude = float(input("Enter exact alitude"))
+    level = float(input("Your altitude level will be between")
 
-    Exosphere == float(input("Your altitude level will be between 700 and 10,000 km "))
-    
+    if 700<=altitude<= 10000:
+     layer = "Exosphere"
+    elif 85<=altiude<= 700:
+     layer = "Thermosphere"
+    elif 50<=altitude<= 85:
+     layer = "Mesosphere"
+    elif 12<=altitude<= 50:
+     layer = "Strastosphere"
+    elif 0<=altitude<= 12:
+     layer = "Troposphere"
+
+
+
 
 
 
