9b415ad1d05d8657cc02c22c41e5174a00a12425
Kingunicornio20ve <qqyjdwtq4n@privaterelay.appleid.com>
2026-08-27 16:38:24 +0000

    test

---
+++ atmosphere.py
@@ -0,0 +1,12 @@
+def main():
+    layer = input("Descent atmosphere layer : ")
+
+    Exosphere == float(input("Your altitude level will be between 700 and 10,000 km "))
+    
+
+
+
+
+
+if __name__ == "__main__":
+    main()
