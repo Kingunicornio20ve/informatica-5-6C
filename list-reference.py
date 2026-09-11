8db5f842045c19286ece6fe4c0b0b7323eb42110
Kingunicornio20ve <qqyjdwtq4n@privaterelay.appleid.com>
2026-09-11 17:30:53 +0000

    teams

--- list-reference.py
+++ list-reference.py
@@ -6,8 +6,9 @@ def main():
     print("con pop(1):", lista)
 
     lista.remove("Rojo")
-    print("con remove(´Rojo´):", lista)
+    print("con remove("Rojo"):", lista)
 
+    #green team
     #max sum min
     numbers = [1,2,3,4,5,6,7,8,9,10,11]
     resultmax = max(numbers)
@@ -16,6 +17,24 @@ def main():
     print(resultmin)
     resultsum = sum(numbers)
     print(resultsum)
+
+    #green team
+    mylist = ["pencil","computer","shirt","phone","paper"]
+    print(len(mylist))
+
+    #blue team
+    numbers = [1,4,5,7,9,3]
+    numbers.sort()
+    print(numbers)
+
+    items = ["lettuce","tomato","bread","jam","mayonaise"]
+    letters.sort(reserve=true)
+    print(letters)
+
+    words = ["banana","pie","apple"]
+    sorted_words = sorted(words,key=len)
+    print
+
 
 
 
