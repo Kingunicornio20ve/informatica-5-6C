34ecdf55f85741c4d6b7ec1a2bc88acbcc4e16dc
Kingunicornio20ve <qqyjdwtq4n@privaterelay.appleid.com>
2026-09-17 17:20:39 +0000

    doctrine

---
+++ for-intro.py
@@ -0,0 +1,9 @@
+def main():
+ doctrine = ["faith","repentance","baptism","recieve the holy ghost","enduring to the end"]
+ print("Doctrine of Christ - 2 Nephi 31 ")
+
+
+
+
+if __name__ == "__main__":
+    main()
