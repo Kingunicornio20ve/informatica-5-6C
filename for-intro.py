ebfb4f0195bbfcc8a6608899166600d74d0b1870
Kingunicornio20ve <qqyjdwtq4n@privaterelay.appleid.com>
2026-09-18 02:57:11 +0000

    tasks

--- for-intro.py
+++ for-intro.py
@@ -1,6 +1,11 @@
 def main():
- doctrine = ["faith","repentance","baptism","recieve the holy ghost","enduring to the end"]
- print("Doctrine of Christ - 2 Nephi 31 ")
+    doctrine = ["faith","repentance","baptism","recieve the holy ghost","enduring to the end"]
+    print("Doctrine of Christ - 2 Nephi 31 ")
+    for principle in doctrine:
+        print(principle)
+
+    for i in range(len(doctrine)):
+        print(f"{i+1}.{doctrine[i]}")
 
 
 
